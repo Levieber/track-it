5a9067d391b77bb9c36d39ac46d5ce36724a3c46
Levi Eber <gblevieber@gmail.com>
2023-05-19 15:07:32 -0300

    remove phosphor-icons and add SVG icons component

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,12 +1,13 @@
 <script lang="ts">
 import TaskTimer from "./TaskTimer.vue";
-import { PhPlay, PhPause } from "@phosphor-icons/vue";
+import IconPlay from "./icons/IconPlay.vue";
+import IconPause from "./icons/IconPause.vue";
 
 export default {
   components: {
     TaskTimer,
-    PhPlay,
-    PhPause,
+    IconPlay,
+    IconPause,
   },
 };
 </script>
@@ -22,7 +23,7 @@ export default {
     <section class="w-full text-center">
       <TaskTimer />
     </section>
-    <button type="submit" class="btn-accent btn flex gap-2 text-white"><PhPlay :size="22" /> Começar</button>
-    <button type="button" class="btn-error btn flex gap-2 text-white"><PhPause :size="22" /> Encerrar</button>
+    <button type="submit" class="btn-accent btn flex gap-2 text-white"><IconPlay /> Começar</button>
+    <button type="button" class="btn-error btn flex gap-2 text-white"><IconPause /> Encerrar</button>
   </form>
 </template>
