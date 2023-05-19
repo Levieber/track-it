8482dd12d382b84d2045ac94d3d957a25cc12f80
Levi Eber <gblevieber@gmail.com>
2023-05-19 15:59:16 -0300

    style,fix: eslint pattern and format/lint

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,24 +2,23 @@
 export default {
   data() {
     return {
-      darkMode: matchMedia("(prefers-dark-mode)").matches
-    }
+      darkMode: matchMedia("(prefers-dark-mode)").matches,
+    };
   },
   methods: {
     toggleTheme() {
-      document.documentElement.dataset.theme = this.darkMode ? "dark" : "light"
-      this.darkMode = !this.darkMode
-    }
-  }
-}
+      document.documentElement.dataset.theme = this.darkMode ? "dark" : "light";
+      this.darkMode = !this.darkMode;
+    },
+  },
+};
 </script>
 
 <template>
-  <header class="flex justify-between items-center p-3 bg-sky-900">
+  <header class="flex items-center justify-between bg-sky-900 p-3">
     <h1 class="text-2xl text-white">Track It</h1>
-    <button class="btn bg-transparent border-white text-white" @click="toggleTheme">
+    <button class="btn border-white bg-transparent text-white" @click="toggleTheme">
       Ativar {{ darkMode ? "modo escuro" : "modo claro" }}
     </button>
   </header>
 </template>
-
