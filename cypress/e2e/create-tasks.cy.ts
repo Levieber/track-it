f93044164efa5d542db02ff8e200ce57e375afc7
Levi Eber <gblevieber@gmail.com>
2023-06-02 16:00:18 -0300

    test: add empty task title test

--- cypress/e2e/create-tasks.cy.ts
+++ cypress/e2e/create-tasks.cy.ts
@@ -6,17 +6,23 @@ describe("Create a bulk of tasks", () => {
 		cy.visit("/");
 
 		cy.fixture("tasks").then(({ tasks }: { tasks: Task[] }) => {
+			const emptyTaskTitle = "Tarefa sem título";
 			cy.get("ul").find("li").should("have.length", 1);
 
 			for (const task of tasks) {
-				cy.data("create-task").type(task.title);
+				if (task.title) {
+					cy.data("create-task").type(task.title);
+				}
+
 				cy.data("start-task").click();
 
 				cy.wait(task.time * 1000);
 
 				cy.data("stop-task").click();
 
-				cy.data("task-title").eq(0).should("have.text", task.title);
+				cy.data("task-title")
+					.eq(0)
+					.should("have.text", task.title || emptyTaskTitle);
 
 				const timeFormatted = timerFormatter(task.time);
 
