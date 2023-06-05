863796f0db6b4b8f63f21449700df397ed3221e4
Levi Eber <gblevieber@gmail.com>
2023-06-05 18:25:24 -0300

    chore: add Cypress Cloud Support

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
+	projectId: "1jzizx",
 	e2e: {
 		baseUrl: "http://localhost:3000/",
 		setupNodeEvents(on, config) {
