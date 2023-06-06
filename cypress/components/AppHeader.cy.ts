1c816e2fadfab168e19d82fbe394b451917159f5
Levi Eber <gblevieber@gmail.com>
2023-06-06 14:49:47 -0300

    test,fix: adding color-scheme conditions

--- cypress/components/AppHeader.cy.ts
+++ cypress/components/AppHeader.cy.ts
@@ -2,28 +2,50 @@ import AppHeader from "@src/components/AppHeader.vue";
 
 describe("<AppHeader/>", () => {
 	it("should be change the theme properly", () => {
+		const isDarkTheme = window.matchMedia(
+			"(prefers-color-scheme: dark)",
+		).matches;
 		const buttonText = "ativar modo";
 
 		cy.mount(AppHeader);
 
 		cy.get("button").as("theme-button");
-
-		cy.get("@theme-button").click();
-
-		cy.get("@theme-button").contains(`${buttonText} escuro`, {
-			matchCase: false,
-		});
-
 		cy.get(":root").as("theme-element");
 
-		cy.get("@theme-element").should("have.attr", "data-theme", "light");
+		if (isDarkTheme) {
+			cy.get("@theme-button").contains(`${buttonText} claro`, {
+				matchCase: false,
+			});
+		} else {
+			cy.get("@theme-button").contains(`${buttonText} escuro`, {
+				matchCase: false,
+			});
+		}
 
 		cy.get("@theme-button").click();
 
-		cy.get("@theme-button").contains(`${buttonText} claro`, {
-			matchCase: false,
-		});
+		if (isDarkTheme) {
+			cy.get("@theme-button").contains(`${buttonText} escuro`, {
+				matchCase: false,
+			});
+		} else {
+			cy.get("@theme-button").contains(`${buttonText} claro`, {
+				matchCase: false,
+			});
+		}
 
-		cy.get("@theme-element").should("have.attr", "data-theme", "dark");
+		cy.get("@theme-element").should(
+			"have.attr",
+			"data-theme",
+			isDarkTheme ? "light" : "dark",
+		);
+
+		cy.get("@theme-button").click();
+
+		cy.get("@theme-element").should(
+			"have.attr",
+			"data-theme",
+			isDarkTheme ? "dark" : "light",
+		);
 	});
 });
